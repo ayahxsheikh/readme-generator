8cea7a59a6e76747126d70e5e055e3407a158119
cc-aeya <aaminah.x@icloud.com>
2023-01-23 08:58:39 +0000

    added arr of questions

--- index.js
+++ index.js
@@ -4,12 +4,50 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+inquirer.prompt([
+{
+    name: 'username',
+    message: 'What is your GitHub username?'
+},
+{
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+{
+    name: 'description',
+    message: 'Please provide a short description of your project'
+},
+{
+    name: 'installation',
+    message: 'What are the steps required to install your project'
+},
+{
+    name: 'usage',
+    message: 'Provide further instructions for usage'
+},
+{
+    name: 'credits',
+    messgae: 'list any collaborators'
+},
+{
+    name: 'license',
+    message: 'What is your license',
+    type: 'list',
+    choices: ['MT',]
+}
+]).then(answers => {
 
-];
+//    [
+//     answers.username, answers.title, answers.description, answers.usage, answers.credits, answers.license
+// ];
+
+console.log(answers);
+})
 
 // function to write README file
-function writeToFile(fileName, data) {
+//append each answer from user input under relevant title in the readme file 
+function writeToFile(fileName, ansers) {
+
 }
 
 // function to initialize program
